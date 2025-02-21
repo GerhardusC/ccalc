018314f4af285b03a27e39c9c901a83e0414f646
Gerhardus Cronje <hardus.cronje@gmail.com>
2025-02-21 08:30:41 +0200

    refactor: moved checking a number to its own function

--- main.c
+++ main.c
@@ -44,8 +44,8 @@ void print_list(struct Token *head);
 
 // -------------------- MAIN ----------------------
 int main () {
-    // struct Token *tokens = tokenise_input("!100+2000*3(1+2)-32");
-    // print_list(tokens);
+    struct Token *tokens = tokenise_input("!100+2000*3(1+2)-32+(1-2-3+4592)*3");
+    print_list(tokens);
     // int num = parse_int("1234651");
     // printf("%d", num);
     // int num1;
@@ -55,7 +55,7 @@ int main () {
     // printf("Enter num 2: \n");
     // scanf("%d", &num2);
     // printf("%d\n", int_pow_to_uint(num1, num2));
-    printf("%d\n", pow_to_uint(5, 4));
+    // printf("%d\n", pow_to_uint(5, 4));
 };
 
 // FUNCTION DEFINITIONS
@@ -76,6 +76,7 @@ int check_valid_character(char symbol){
 }
 
 
+
 struct Token* create_head(char val[], enum TokenKind kind){
     struct Token *new_node = malloc(sizeof(struct Token));
     new_node->head = new_node;
@@ -93,6 +94,35 @@ struct Token* create_node(char val[], enum TokenKind kind){
     return new_node;
 }
 
+enum RespondToNumOutcome {
+    NOT_A_NUMBER,
+    CONTINUE,
+    RETURN,
+};
+
+enum RespondToNumOutcome respond_to_number(char curr, char next, char *current_tok, int *cur_iter, int *cur_len, struct Token *last_token){
+    if((int) curr >= (int) '0' && (int) curr <= (int) '9'){
+        current_tok[*cur_iter] = curr;
+        *cur_len = *cur_len + 1;
+        *cur_iter = *cur_iter + 1;
+        if(next == '\0'){
+            char num_contents[*cur_len + 1];
+
+            for(int j = 0; j < *cur_len; j++){
+                num_contents[j] = current_tok[j];
+                current_tok[j] = '\0';
+            }
+            num_contents[*cur_len] = '\0';
+            struct Token *new_tok_for_num = create_node(num_contents, NUM);
+            insert_after(last_token, new_tok_for_num);
+            last_token = new_tok_for_num;
+            return RETURN;
+        }
+        return CONTINUE;
+    }
+    return NOT_A_NUMBER;
+}
+
 struct Token* tokenise_input(char *input){
     struct Token *head = create_head("head", INVALID);
     struct Token *last_token = head;
@@ -106,26 +136,18 @@ struct Token* tokenise_input(char *input){
         i++;
         // Skip whitespace
         if(curr == ' ') continue;
-        // Input is a number.
-        if((int) curr >= (int) '0' && (int) curr <= (int) '9'){
-            current_tok[c_i] = curr;
-            c_l++;
-            c_i++;
-            if(input[i] == '\0'){
-                char num_contents[c_l + 1];
 
-                for(int j = 0; j < c_l; j++){
-                    num_contents[j] = current_tok[j];
-                    current_tok[j] = '\0';
-                }
-                num_contents[c_l] = '\0';
-                struct Token *new_tok_for_num = create_node(num_contents, NUM);
-                insert_after(last_token, new_tok_for_num);
-                last_token = new_tok_for_num;
+        enum RespondToNumOutcome outcome = respond_to_number(curr, input[i], current_tok, &c_i, &c_l, last_token);
+
+        switch (outcome){
+            case RETURN:
                 return head;
-            }
-            continue;
+            case CONTINUE:
+                continue;
+            case NOT_A_NUMBER:
+                break;
         }
+
         if(check_valid_character(curr) == 0) continue;
         // create new node for number
         char num_contents[c_l + 1];
